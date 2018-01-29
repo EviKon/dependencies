773bb159733784cded1b5683112c5cdeaca0e1c6
Evi-Konstantoula <Paraskevi.Konstantoula@onthebeach.co.uk>
2018-01-29 15:19:08 +0000

    some refactoring

--- lib/code.rb
+++ lib/code.rb
@@ -1,5 +1,8 @@
 class Code
   attr_reader :input
+  attr_reader :first
+  attr_reader :hash
+  attr_reader :string
 
   def initialize(input)
     @input = input
@@ -14,59 +17,68 @@ class Code
     new_array.to_h
   end
 
-  def iterate_the_hash
-    hash = convert_to_hash
-    string = ''
-    hash.each_key do |x|
-      string.concat(x)
-    end
-    string
-  end
-
-  def iterate_the_hash_with_keys_and_values
-    hash = convert_to_hash
-    string = ''
+  def jobs
+    @hash = convert_to_hash
+    @string = ''
     hash.each do |k,v|
-      p k
-      p v
-      string.concat(checking_the_hash(k,v,string))
-      p string
+      raise error if k == v
+
+      @first = k
+      string.concat(checking_the_hash(k,v))
     end
     string
   end
 
-  def check_value(v)
-    hash = convert_to_hash
-
-    hash.each_key do |key|
-      if key == v
-        return true
-      end
-    end
-    false
-  end
-
-  def checking_the_hash(key,v,string)
-    if v != ' ' && !string.include?(key)
-      if check_value(v)
-        new_value = get_key(v)
-        checking_the_hash(v,new_value,string)+key
-      else
-        v+key
-      end
-    elsif !string.include?(key) && v == ' '
+  def checking_the_hash(key,v)
+    if job_has_dependancy(key,v)
+      dependency(key,v)
+    elsif job_has_no_dependancy(key,v)
       key
     else
       ''
     end
   end
 
-  def get_key(v)
-    hash = convert_to_hash
-    hash.each do |key,value|
-      if key == v
-        return value
-      end
+  def job_has_dependancy(key,v)
+    v != ' ' && !string.include?(key)
+  end
+
+  def job_has_no_dependancy(key,v)
+    !string.include?(key) && v == ' '
+  end
+
+  def dependency(key,v)
+    if check_value(v)
+      check_circularity(v)
+
+      checking_the_hash(v,get_new_value(v)) + key
+    else
+      v + key
     end
+  end
+
+  def check_value(v)
+    hash.each_key do |key|
+      return true if key == v
+    end
+    false
+  end
+
+  def check_circularity(v)
+    raise circular_error if get_new_value(v) == first
+  end
+
+  def get_new_value(v)
+    hash.each do |key,value|
+      return value if key == v
+    end
+  end
+
+  def error
+    "There can not be self dependencies"
+  end
+
+  def circular_error
+    "There can not be circular dependencies"
   end
 end
--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -4,44 +4,27 @@ RSpec.describe Code do
   let(:input) { 'a=> ' }
   let(:instance) { Code.new(input) }
 
-  describe '#convert_to_hash' do
-    subject { instance.convert_to_hash }
+  describe '#jobs' do
+    subject { instance.jobs }
 
     it 'returns a hash of the input string' do
-      expect(subject).to eql({ 'a' => ' '})
-    end
-
-    context 'when the input has more than one dependencies' do
-      let(:input) { 'a=> ,b=> ' }
-
-      it 'returns a hash of the input string with multiple keys' do
-        expect(subject).to eql({ 'a' => ' ', 'b'=> ' ' })
-      end
-    end
-  end
-
-  describe '#iterate_the_hash_with_keys' do
-    subject { instance.iterate_the_hash }
-
-    it 'returns every the key of the hash in a string' do
       expect(subject).to eql('a')
     end
 
     context 'when the input has more than one dependencies' do
       let(:input) { 'a=> ,b=> ' }
 
-      it 'returns every the key of the hash in a string' do
+      it 'returns a hash of the input string with multiple keys' do
         expect(subject).to eql('ab')
       end
     end
-  end
 
-  describe '#iterate_the_hash_with_keys_and_values' do
-    let(:input) { 'a=>c,b=>d' }
-    subject { instance.iterate_the_hash_with_keys_and_values }
+    context '' do
+      let(:input) { 'a=>c,b=>d' }
 
-    it 'returns the output with the values before the keys' do
-      expect(subject).to eql('cadb')
+      it 'returns the output with the values before the keys' do
+        expect(subject).to eql('cadb')
+      end
     end
 
     context 'when the input has dependencies on other keys' do
@@ -57,6 +40,22 @@ RSpec.describe Code do
 
       it 'returns the output with the jobs in the right order' do
         expect(subject).to eql('afcbde')
+      end
+    end
+
+    context 'when the input contains a self dependency' do
+      let(:input) { 'a=> ,b=> ,c=>c' }
+
+      it 'raises an error for self dependencies' do
+        expect{ subject }.to raise_error "There can not be self dependencies"
+      end
+    end
+
+    context 'when the input contains a circular dependency' do
+      let(:input) { 'a=> ,b=>c,c=>f,d=>a,e=> ,f=>b' }
+
+      it 'raises an error for circular dependencies' do
+        expect{ subject }.to raise_error "There can not be circular dependencies"
       end
     end
   end
